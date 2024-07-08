1133978944bfc39f0baa1a9e443c1b95074d9b4e
MiguelCastro9 <miguel.castro9@hotmail.com>
2024-07-08 11:12:25 -0300

    add CircuitBreaker in micro-service movie-recommendation-service

--- movie-recommendation-service/src/main/java/com/api/controller/MovieRecommendationController.java
+++ movie-recommendation-service/src/main/java/com/api/controller/MovieRecommendationController.java
@@ -2,6 +2,7 @@ package com.api.controller;
 
 import com.api.dto.response.MovieResponseDto;
 import com.api.proxy.MovieProxy;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +23,23 @@ public class MovieRecommendationController {
     MovieProxy movieProxy;
 
     @GetMapping("/recommendations/{gender}")
+    /**
+     * @CircuitBreaker annotation so that in case of endpoint error it tries
+     * again without throwing an exception, by default it tries the call 3 times
+     * before throwing the exception, there are several customizations for this
+     * type of situation, just check the documentation, this is very good for
+     * management many requests in the endpoint.
+     */
+    @CircuitBreaker(name = "default", fallbackMethod = "messageFallback")
     public List<MovieResponseDto> getMovies(@PathVariable("gender") String gender) {
 
         List<MovieResponseDto> recommendations = movieProxy.getMovies().stream()
                 .filter(movie -> movie.getGender().equalsIgnoreCase(gender))
                 .collect(Collectors.toList());
         return recommendations;
+    }
+
+    public String messageFallback(Exception ex) {
+        return "service currently unavailable!";
     }
 }
