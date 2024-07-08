2a095df309026438caab471d36a70f5f8fd9a08f
MiguelCastro9 <miguel.castro9@hotmail.com>
2024-07-08 06:32:52 -0300

    filter data by gender in micro-service movie-recomendation-service

--- movie-recommendation-service/src/main/java/com/api/controller/MovieRecommendationController.java
+++ movie-recommendation-service/src/main/java/com/api/controller/MovieRecommendationController.java
@@ -3,8 +3,10 @@ package com.api.controller;
 import com.api.dto.response.MovieResponseDto;
 import com.api.proxy.MovieProxy;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +21,12 @@ public class MovieRecommendationController {
     @Autowired
     MovieProxy movieProxy;
 
-    @GetMapping("/recommendations")
-    public List<MovieResponseDto> getMovies() {
+    @GetMapping("/recommendations/{gender}")
+    public List<MovieResponseDto> getMovies(@PathVariable("gender") String gender) {
 
-        List<MovieResponseDto> recommendations = movieProxy.getMovies();
+        List<MovieResponseDto> recommendations = movieProxy.getMovies().stream()
+                .filter(movie -> movie.getGender().equalsIgnoreCase(gender))
+                .collect(Collectors.toList());
         return recommendations;
     }
 }
