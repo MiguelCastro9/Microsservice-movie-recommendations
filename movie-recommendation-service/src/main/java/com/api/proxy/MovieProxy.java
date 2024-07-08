c87cd69bea815244ababfcd028f3c9429b816c77
MiguelCastro9 <miguel.castro9@hotmail.com>
2024-07-08 06:49:17 -0300

    add eureka server for load balance

--- movie-recommendation-service/src/main/java/com/api/proxy/MovieProxy.java
+++ movie-recommendation-service/src/main/java/com/api/proxy/MovieProxy.java
@@ -9,7 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
  *
  * @author Miguel Castro
  */
-@FeignClient(name = "movie-data-service", url = "http://localhost:8080")
+/**
+ * using the eureka server, basically each instance that goes up automatically 
+ * registers with the 'naming server', so the book-service needs to communicate 
+ * with the exchange-service, and what it does is to ask the 'naming server' 
+ * which addresses the exchange-service is available and it returns this information 
+ * and it can balance the load between each of them, eureka knows where to make 
+ * the request and how to balance the load.
+ */
+@FeignClient(name = "movie-data-service")
 public interface MovieProxy {
     
     @GetMapping("/api/v1/movies")
